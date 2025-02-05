e4407bca34a860cb5ce380b234e42ace44d5a7b8
Kronik502 <Kronikwenaite@gmail.com>
2025-02-05 18:12:24 +0200

    updates

--- src/components/About.js
+++ src/components/About.js
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-// src/components/About.js
-import React from 'react';
-import './About.css';
-import image from '../components/Profile.jpg'
-
-function About() {
-  return (
-    <section id="about" className="about">
-      <div className="profile-pic">
-        <img src={image} alt="Profile" />
-      </div>
-      <div className="about-content">
-        <h2>About Me</h2>
-        <p>Creative and innovative web developer with a strong foundation in front-end technologies and
-a focus on enhancing user experience through efficient, visually appealing designs. Skilled in
-ReactJS, Redux, NodeJS, Firebase & MongoDB, with experience developing interactive web
-applications, including hotel and marketplace platforms. Known for creating professional and
-user-centric interfaces, designing unique CSS Bootsrap & Tailwind styles, and employing
-responsive, accessible layouts.
-Proven ability to manage complex projects, work collaboratively, and deliver high-quality code.
-Proficient in multi-functional application features such as user authentication, product
-management, and custom search functionalities. Passionate about technology and
-continuous learning in development and design.</p>
-        <p>Fun Fact: Deeply interested in gaming, including retro platforms, with a focus on technical aspects and game design.</p>
-=======
 import React, { useEffect, useState } from 'react';
 import './About.css';
 import image from '../images/Profiler.jpg';
@@ -61,7 +35,6 @@ function About() {
             Fun Fact: I'm a gamer at heart with a strong passion for retro games and their design. I also enjoy creating innovative applications like hotel booking platforms, marketplace apps, and more.
           </p>
         </div>
->>>>>>> master
       </div>
     </section>
   );
